5b7c510d8e85e7d952b6a5a6be621c7e9ef45111
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-10-28 12:04:45 +0330

    initialize bot from template

--- src/run.py
+++ src/run.py
@@ -2,7 +2,8 @@ import emoji
 from loguru import logger
 
 from src.bot import bot
-from src.constants import keyboards
+from src.constants import keyboards, keys, states
+from src.db import db
 from src.filters import IsAdmin
 
 
@@ -10,10 +11,11 @@ class Bot:
     """
     Telegram bot to randomly connect two strangers to talk!
     """
-    def __init__(self, telebot):
+    def __init__(self, telebot, mongodb):
         self.bot = telebot
+        self.db = mongodb
 
-        # add mustom filters
+        # add custom filters
         self.bot.add_custom_filter(IsAdmin())
 
         # register handlers
@@ -24,9 +26,28 @@ class Bot:
         self.bot.infinity_polling()
 
     def handlers(self):
+        @self.bot.message_handler(commands=['start'])
+        def start(message):
+            """
+            /start command handler.
+            """
+            self.bot.send_message(
+                message.chat.id,
+                f"Hey <strong>{message.chat.first_name}</strong>",
+                reply_markup=keyboards.main
+            )
+
+            self.db.users.update_one(
+                {'chat.id': message.chat.id},
+                {'$set': message.json},
+                upsert=True
+            )
+            self.update_state(message.chat.id, states.main)
+
         @self.bot.message_handler(is_admin=True)
         def admin_of_group(message):
-            self.send_message(message.chat.id, '<strong> You are admin of this group! </strong>')
+            self.send_message(message.chat.id, '<strong> You are admin of this\
+                              group! </strong>')
 
         @self.bot.message_handler(func=lambda _: True)
         def echo(message):
@@ -37,15 +58,24 @@ class Bot:
 
     def send_message(self, chat_id, text, reply_markup=None, emojize=True):
         """
-        Send message to telegram bot. 
+        Send message to telegram bot.
         """
         if emojize:
             text = emoji.emojize(text, use_aliases=True)
 
         self.bot.send_message(chat_id, text, reply_markup=reply_markup)
 
+    def update_state(self, chat_id, state):
+        """
+        Update user state.
+        """
+        self.db.users.update_one(
+            {'chat.id': chat_id},
+            {'$set': {'state': state}}
+        )
+
 
 if __name__ == '__main__':
     logger.info('Bot Started')
-    nashenas_bot = Bot(telebot=bot)
+    nashenas_bot = Bot(telebot=bot, mongodb=db)
     nashenas_bot.run()
